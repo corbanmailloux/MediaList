a7aef03d5813ac4c9f2dafa8d59fb5be411cc76a
corbanmailloux <cdm3806@rit.edu>
2014-05-05 16:26:27 -0400

    StyleCop-ed OptionsWindow.xaml.cs. All code passes StyleCop.

--- MediaList/MediaList/OptionsWindow.xaml.cs
+++ MediaList/MediaList/OptionsWindow.xaml.cs
@@ -1,56 +1,67 @@
-﻿using System;
-using System.Windows;
-
+﻿// <copyright file="OptionsWindow.xaml.cs" company="corb.co">
+//     corb.co. All rights reserved.
+// </copyright>
+// <author>Corban Mailloux</author>
 namespace MediaList
 {
+    using System;
+    using System.Windows;
+
     /// <summary>
-    /// Interaction logic for OptionsWindow.xaml
+    /// Interaction logic for the OptionsWindow
     /// </summary>
     public partial class OptionsWindow : Window
     {
-        /*
-         * Keep the MainWindow around.
-         */
+        /// <summary>
+        /// Keep the MainWindow around.
+        /// </summary>
         private MainWindow mainWindow;
 
-        /*
-         * Create the "Options" window. 
-         * At the moment, it only has an exclude files section.
-         */
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OptionsWindow"/> class.
+        /// Create the "Options" window.
+        /// At the moment, it only has an exclude files section.
+        /// </summary>
+        /// <param name="mainWindow">The MainWindow that created this.</param>
         public OptionsWindow(MainWindow mainWindow)
         {
             this.mainWindow = mainWindow;
-            InitializeComponent();
+            this.InitializeComponent();
 
             // Set up the listbox
             ExcludedFilesListBox.ItemsSource = Properties.Settings.Default.ExcludedFiles;
         }
 
-        /*
-         * Force the listbox to update.
-         */
+        /// <summary>
+        /// Force the list box to update.
+        /// </summary>
         private void UpdateList()
         {
             ExcludedFilesListBox.Items.Refresh();
         }
 
-        /*
-         * Save the list and update MainWindow before closing.
-         */
+        /// <summary>
+        /// Save the list and update MainWindow before closing.
+        /// </summary>
+        /// <param name="sender">The caller.</param>
+        /// <param name="e">The arguments.</param>
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             Properties.Settings.Default.Save();
-            mainWindow.SetUpMovieFolders();
+            this.mainWindow.SetUpMovieFolders();
+
             // This doesn't affect TV Shows at the moment.
-            //mainWindow.SetUpTVFolders();
+            ////mainWindow.SetUpTVFolders();
         }
 
-        /*
-         * Add the text from the text box to the list.
-         */
+        /// <summary>
+        /// Add the text from the text box to the list.
+        /// </summary>
+        /// <param name="sender">The caller.</param>
+        /// <param name="e">The arguments.</param>
         private void AddExcludedFileButton_Click(object sender, RoutedEventArgs e)
         {
-            if (!String.IsNullOrEmpty(ExcludedFileTextBox.Text))
+            if (!string.IsNullOrEmpty(ExcludedFileTextBox.Text))
             {
                 if (Properties.Settings.Default.ExcludedFiles.Contains(ExcludedFileTextBox.Text))
                 {
@@ -59,20 +70,23 @@ namespace MediaList
                 else
                 {
                     Properties.Settings.Default.ExcludedFiles.Add(ExcludedFileTextBox.Text);
-                    UpdateList();
+                    this.UpdateList();
                 }
             }
         }
 
-        /*
-         * Remove the selected file name from the list of exclusions.
-         */
+        /// <summary>
+        /// Remove the selected file name from the list of exclusions.
+        /// </summary>
+        /// <param name="sender">The caller.</param>
+        /// <param name="e">The arguments.</param>
         private void RemoveExcludedFileButton_Click(object sender, RoutedEventArgs e)
         {
-            if (ExcludedFilesListBox.SelectedIndex != -1) // Ensure that there is a selection.
+            // Ensure that there is a selection.
+            if (ExcludedFilesListBox.SelectedIndex != -1) 
             {
                 Properties.Settings.Default.ExcludedFiles.RemoveAt(ExcludedFilesListBox.SelectedIndex);
-                UpdateList();
+                this.UpdateList();
             }
         }
     }
