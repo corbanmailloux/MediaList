9b77b5a830000e722217351e51895dce203c6d2b
corbanmailloux <cdm3806@rit.edu>
2014-05-05 16:19:58 -0400

    StyleCop-ed MainWindow.xaml.cs

--- MediaList/MediaList/MainWindow.xaml.cs
+++ MediaList/MediaList/MainWindow.xaml.cs
@@ -1,84 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-
+﻿// <copyright file="MainWindow.xaml.cs" company="corb.co">
+//     corb.co. All rights reserved.
+// </copyright>
+// <author>Corban Mailloux</author>
 namespace MediaList
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for the Main Window
     /// </summary>
     public partial class MainWindow : Window
     {
-        // These should probably be moved into another class...
+        /// <summary>
+        /// The total list of Movies.
+        /// </summary>
         private List<Movie> movieList = new List<Movie>();
+
+        /// <summary>
+        /// The total list of TVShows.
+        /// </summary>
         private List<TVShow> tvList = new List<TVShow>();
 
-        // The list(s) currently displayed.
+        /// <summary>
+        /// The Movies currently in the list.
+        /// </summary>
         private List<Movie> currMovieList = new List<Movie>();
+
+        /// <summary>
+        /// The TVShows currently in the list.
+        /// </summary>
         private List<TVShow> currTVList = new List<TVShow>();
 
-        /*
-         * Build the main window, causing the initial indexing.
-         */
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// Build the main window, causing the initial indexing.
+        /// </summary>
         public MainWindow()
         {
-            InitializeComponent();
-
-            SetUpExcludedFiles();
-            SetUpMovieFolders();
-            SetUpTVFolders();
-            MovieListBox.ItemsSource = currMovieList;
-            TVListBox.ItemsSource = currTVList;
+            this.InitializeComponent();
+            this.SetUpExcludedFiles();
+            this.SetUpMovieFolders();
+            this.SetUpTVFolders();
+            MovieListBox.ItemsSource = this.currMovieList;
+            TVListBox.ItemsSource = this.currTVList;
         }
 
-        /*
-         * Close this window when the "Exit" menu item is selected.
-         */
-        private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
-
-        /*
-         * Create and show the "Add or Remove Folders" window.
-         */
-        private void ChangeFolderMenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            new FolderDialog(this).Show();
-        }
-
-        /*
-         * Create and show the "Options" window.
-         */
-        private void OptionsMenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            new OptionsWindow(this).Show();
-        }
-
-        /*
-         * Ensure that the excluded file list is created.
-         */
-        private void SetUpExcludedFiles()
-        {
-            if (Properties.Settings.Default.ExcludedFiles == null)
-            {
-                Properties.Settings.Default.ExcludedFiles = new System.Collections.Specialized.StringCollection();
-            }
-        }
-
-
-        // MOVIE
-
-        /*
-         * Go through each of the Movie folders, indexing each Movie.
-         */
+        /// <summary>
+        /// Go through each of the Movie folders, indexing each Movie.
+        /// </summary>
         public void SetUpMovieFolders()
         {
-            movieList.Clear();
-            currMovieList.Clear();
+            this.movieList.Clear();
+            this.currMovieList.Clear();
 
             if (Properties.Settings.Default.MovieFolders == null || Properties.Settings.Default.MovieFolders.Count == 0)
             {
@@ -86,7 +64,7 @@ namespace MediaList
                 Properties.Settings.Default.MovieFolders = new System.Collections.Specialized.StringCollection();
             }
 
-            foreach (String inStr in Properties.Settings.Default.MovieFolders)
+            foreach (string inStr in Properties.Settings.Default.MovieFolders)
             {
                 DirectoryInfo di = new DirectoryInfo(inStr);
                 
@@ -97,28 +75,103 @@ namespace MediaList
                         // Ensure excluded files are... excluded.
                         if (!Properties.Settings.Default.ExcludedFiles.Contains(inFile.Name))
                         {
-                            movieList.Add(new Movie(inFile));
+                            this.movieList.Add(new Movie(inFile));
                         }
                     }
                 }
             }
-            movieList.Sort();
-            currMovieList.AddRange(movieList);
-            UpdateMovieList();
+
+            this.movieList.Sort();
+            this.currMovieList.AddRange(this.movieList);
+            this.UpdateMovieList();
         }
 
-        /*
-         * Force the Movie list box to update, also updating the status bar.
-         */
+        /// <summary>
+        /// Go through each of the TV folders, indexing each TV folder.
+        /// </summary>
+        public void SetUpTVFolders()
+        {
+            this.tvList.Clear();
+            this.currTVList.Clear();
+
+            if (Properties.Settings.Default.TVFolders == null || Properties.Settings.Default.TVFolders.Count == 0)
+            {
+                MessageBox.Show("No TV folders are selected to search.\nYou can add folders through the \"Edit\" menu.");
+                Properties.Settings.Default.TVFolders = new System.Collections.Specialized.StringCollection();
+            }
+
+            foreach (string inStr in Properties.Settings.Default.TVFolders)
+            {
+                DirectoryInfo di = new DirectoryInfo(inStr);
+
+                if (di.Exists)
+                {
+                    foreach (DirectoryInfo inDir in di.EnumerateDirectories())
+                    {
+                        // TODO: Do some checking if this is a TV folder?
+                        this.tvList.Add(new TVShow(inDir));
+                    }
+                }
+            }
+
+            this.tvList.Sort();
+            this.currTVList.AddRange(this.tvList);
+            this.UpdateTVList();
+        }
+        
+        /// <summary>
+        /// Close the window when the "Exit" menu item is selected.
+        /// </summary>
+        /// <param name="sender">The caller.</param>
+        /// <param name="e">The arguments.</param>
+        private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Create and show the "Add or Remove Folders" window.
+        /// </summary>
+        /// <param name="sender">The caller.</param>
+        /// <param name="e">The arguments.</param>
+        private void ChangeFolderMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            new FolderDialog(this).Show();
+        }
+
+        /// <summary>
+        /// Create and show the "Options" window.
+        /// </summary>
+        /// <param name="sender">The caller.</param>
+        /// <param name="e">The arguments.</param>
+        private void OptionsMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            new OptionsWindow(this).Show();
+        }
+
+        /// <summary>
+        /// Ensure that the excluded file list is created.
+        /// </summary>
+        private void SetUpExcludedFiles()
+        {
+            if (Properties.Settings.Default.ExcludedFiles == null)
+            {
+                Properties.Settings.Default.ExcludedFiles = new System.Collections.Specialized.StringCollection();
+            }
+        }
+
+        /// <summary>
+        /// Force the Movie list box to update, also updating the status bar.
+        /// </summary>
         private void UpdateMovieList()
         {
             MovieListBox.Items.Refresh();
-            MovieStatusBarText.Text = "Number of Movies: " + currMovieList.Count.ToString();
+            MovieStatusBarText.Text = "Number of Movies: " + this.currMovieList.Count.ToString();
         }
 
-        /*
-         * Open the currently selected movie in Windows Explorer.
-         */
+        /// <summary>
+        /// Open the currently selected movie in Windows Explorer.
+        /// </summary>
         private void OpenSelectedMovieInExplorer()
         {
             if (MovieListBox.SelectedIndex != -1)
@@ -128,114 +181,90 @@ namespace MediaList
             }
         }
 
-        /*
-         * Add a listener to add real-time searching.
-         * 
-         * Not the most elegant or efficient solution, but it works.
-         * Ideally, some kind of filtering would happen, instead of rebuilding the 
-         *  whole list each time the search text changes.
-         */
+        /// <summary>
+        /// Add a listener to add real-time searching.
+        /// </summary>
+        /// <param name="sender">The caller.</param>
+        /// <param name="e">The arguments.</param>
         private void MovieSearchBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            currMovieList.Clear();
-            foreach (Movie m in movieList)
+            /*
+             * Not the most elegant or efficient solution, but it works.
+             * Ideally, some kind of filtering would happen, instead of rebuilding the 
+             *  whole list each time the search text changes.
+             */
+
+            this.currMovieList.Clear();
+            foreach (Movie m in this.movieList)
             {
                 if (m.Name().ToLower().Contains(MovieSearchBox.Text.ToLower()))
                 {
-                    currMovieList.Add(m);
+                    this.currMovieList.Add(m);
                 }
             }
+
             // Force an update.
-            UpdateMovieList();
+            this.UpdateMovieList();
         }
 
-        /*
-         * Catch when an item is double-clicked to open it in explorer.
-         */
+        /// <summary>
+        /// Catch when an item is double-clicked to open it in explorer.
+        /// </summary>
+        /// <param name="sender">The caller.</param>
+        /// <param name="e">The arguments.</param>
         private void MovieListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            OpenSelectedMovieInExplorer();
+            this.OpenSelectedMovieInExplorer();
         }
 
-        /*
-         * Catch when the Enter key is pressed to open selection in explorer.
-         */
+        /// <summary>
+        /// Catch when the Enter key is pressed to open selection in explorer.
+        /// </summary>
+        /// <param name="sender">The caller.</param>
+        /// <param name="e">The arguments.</param>
         private void MovieListBox_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
             {
-                OpenSelectedMovieInExplorer();
+                this.OpenSelectedMovieInExplorer();
             }
         }
 
-        /*
-         * Update the Name, Path, and Size boxes when a new Movie is selected.
-         */
+        /// <summary>
+        /// Update the Name, Path, and Size boxes when a new Movie is selected.
+        /// </summary>
+        /// <param name="sender">The caller.</param>
+        /// <param name="e">The arguments.</param>
         private void MovieListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (MovieListBox.SelectedIndex == -1)
             {
-                MovieNameBox.Text = String.Empty;
-                MoviePathBox.Text = String.Empty;
-                MovieSizeBox.Text = String.Empty;
+                MovieNameBox.Text = string.Empty;
+                MoviePathBox.Text = string.Empty;
+                MovieSizeBox.Text = string.Empty;
             }
             else
             {
                 MovieNameBox.Text = ((Movie)MovieListBox.SelectedItem).Name();
                 MoviePathBox.Text = ((Movie)MovieListBox.SelectedItem).Path();
                 double size = ((Movie)MovieListBox.SelectedItem).Size();
-                size = size / (1073741824.0); // Convert bytes to GB
+                size = size / 1073741824.0; // Convert bytes to GB
                 MovieSizeBox.Text = Math.Round(size, 2).ToString() + " GB";
             }
         }
 
-
-        // TV
-
-        /*
-         * Go through each TV folder, indexing each TV show.
-         */
-        public void SetUpTVFolders()
-        {
-            tvList.Clear();
-            currTVList.Clear();
-
-            if (Properties.Settings.Default.TVFolders == null || Properties.Settings.Default.TVFolders.Count == 0)
-            {
-                MessageBox.Show("No TV folders are selected to search.\nYou can add folders through the \"Edit\" menu.");
-                Properties.Settings.Default.TVFolders = new System.Collections.Specialized.StringCollection();
-            }
-
-            foreach (String inStr in Properties.Settings.Default.TVFolders)
-            {
-                DirectoryInfo di = new DirectoryInfo(inStr);
-
-                if (di.Exists)
-                {
-                    foreach (DirectoryInfo inDir in di.EnumerateDirectories())
-                    {
-                        // TODO: Do some checking if this is a TV folder?
-                        tvList.Add(new TVShow(inDir));
-                    }
-                }
-            }
-            tvList.Sort();
-            currTVList.AddRange(tvList);
-            UpdateTVList();
-        }
-
-        /*
-         * Force the TV list box to update, also updating the status bar.
-         */
+        /// <summary>
+        /// Force the TV list box to update, also updating the status bar.
+        /// </summary>
         private void UpdateTVList()
         {
             TVListBox.Items.Refresh();
-            TVStatusBarText.Text = "Number of TV Shows: " + currTVList.Count.ToString();
+            TVStatusBarText.Text = "Number of TV Shows: " + this.currTVList.Count.ToString();
         }
 
-        /*
-         * Open the currently selected TV show in Windows Explorer.
-         */
+        /// <summary>
+        /// Open the currently selected TV show in Windows Explorer.
+        /// </summary>
         private void OpenSelectedTVShowInExplorer()
         {
             if (TVListBox.SelectedIndex != -1)
@@ -245,61 +274,75 @@ namespace MediaList
             }
         }
 
-        /*
-         * Add a listener to add real-time searching.
-         */
+        /// <summary>
+        /// Add a listener to add real-time searching.
+        /// </summary>
+        /// <param name="sender">The caller.</param>
+        /// <param name="e">The arguments.</param>
         private void TVSearchBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            currTVList.Clear();
-            foreach (TVShow show in tvList)
+            this.currTVList.Clear();
+            foreach (TVShow show in this.tvList)
             {
                 if (show.Name().ToLower().Contains(TVSearchBox.Text.ToLower()))
                 {
-                    currTVList.Add(show);
+                    this.currTVList.Add(show);
                 }
             }
+
             // Force an update.
-            UpdateTVList();
+            this.UpdateTVList();
         }
 
-        /*
-         * Catch when an item is double-clicked to open it in explorer.
-         */
+        /// <summary>
+        /// Catch when an item is double-clicked to open it in explorer.
+        /// </summary>
+        /// <param name="sender">The caller.</param>
+        /// <param name="e">The arguments.</param>
         private void TVListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            OpenSelectedTVShowInExplorer();
+            this.OpenSelectedTVShowInExplorer();
         }
 
-        /*
-         * Catch when the Enter key is pressed to open selection in explorer.
-         */
+        /// <summary>
+        /// Catch when the Enter key is pressed to open selection in explorer.
+        /// </summary>
+        /// <param name="sender">The caller.</param>
+        /// <param name="e">The arguments.</param>
         private void TVListBox_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
             {
-                OpenSelectedTVShowInExplorer();
+                this.OpenSelectedTVShowInExplorer();
             }
         }
-
-        /*
-         * Update the Name, Path, and Newest Episode boxes when a new TVShow is selected.
-         */
+        
+        /// <summary>
+        /// Update the Name, Path, and Newest Episode boxes when a new TVShow is selected.
+        /// </summary>
+        /// <param name="sender">The caller.</param>
+        /// <param name="e">The arguments.</param>
         private void TVListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (TVListBox.SelectedIndex == -1)
             {
-                TVNameBox.Text = String.Empty;
-                TVPathBox.Text = String.Empty;
-                TVNewestEpisodeBox.Text = String.Empty;
+                TVNameBox.Text = string.Empty;
+                TVPathBox.Text = string.Empty;
+                TVNewestEpisodeBox.Text = string.Empty;
             }
             else
             {
                 TVNameBox.Text = ((TVShow)TVListBox.SelectedItem).Name();
                 TVPathBox.Text = ((TVShow)TVListBox.SelectedItem).Path();
-                TVNewestEpisodeBox.Text = ((TVShow)TVListBox.SelectedItem).NewestEpisode(); ;
+                TVNewestEpisodeBox.Text = ((TVShow)TVListBox.SelectedItem).NewestEpisode();
             }
         }
 
+        /// <summary>
+        /// Generate a MessageBox with the missing episodes.
+        /// </summary>
+        /// <param name="sender">The caller.</param>
+        /// <param name="e">The arguments.</param>
         private void TVCheckMissingEpisodesMenuItem_Click(object sender, RoutedEventArgs e)
         {
             if (TVListBox.SelectedIndex != -1)
@@ -308,6 +351,11 @@ namespace MediaList
             }
         }
 
+        /// <summary>
+        /// Generate a MessageBox with the extensions used.
+        /// </summary>
+        /// <param name="sender">The caller.</param>
+        /// <param name="e">The arguments.</param>
         private void TVSeeFileExtensionsUsedMenuItem_Click(object sender, RoutedEventArgs e)
         {
             if (TVListBox.SelectedIndex != -1)
